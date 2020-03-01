187889523a666ee967a36e0f4594f0db1ae74d93
Marcin Zakrzewski <zakrzewskim2@student.mini.pw.edu.pl>
2020-03-01 01:15:38 +0100

    data read from file; results tested on test set
    Signed-off-by: Marcin Zakrzewski <zakrzewskim2@student.mini.pw.edu.pl>

--- NN.py
+++ NN.py
@@ -13,34 +13,31 @@ def error_function(y, y_exp, derivative = False):
 
 def out_activation(x, derivative=False):
     if (derivative == True):
-        return 1 #x * (1 - x)
+        return 1 
     else:
-        return x #1 / (1 + np.exp(-x))
+        return x 
 #%%
 import numpy as np
+import pandas as pd
 
+train = pd.read_csv("data\classification\data.simple.train.100.csv")
+test = pd.read_csv("data\classification\data.simple.test.100.csv")
+X_train = train.iloc[:,:-1]
+y_train = train["cls"]
+X_test = np.array(test.iloc[:,:-1])
+y_test = np.array([test["cls"]]).T
 # inputs - each row is an input to MLP
-X = np.array([  
-    [0, 0, 1],
-    [0, 1, 1],
-    [1, 1, 1],
-])
-
+X = np.array(X_data)
+# outputs - each row is an expected output for the corresponding input
+y = np.array([y_data]).T
 # N - number of input vectors
 N = X.shape[0]
 
-# outputs - each row is an expected output for the corresponding input
-y = np.array([  
-    [1, 0],
-    [0, 1],
-    [0, 0],
-])
-
 # number of layers (all of them!)
-num_layers = 4
+num_layers = 5
 
 # number of nodes in each layer
-layer_lengths = np.array([X.shape[1], 4, 5, y.shape[1]])
+layer_lengths = np.array([X.shape[1], 5, 5, 3, y.shape[1]])
 if layer_lengths.shape[0] != num_layers:
     print("Error! Number of layers undefined!")
 
@@ -52,11 +49,11 @@ for i in range(0, num_layers-1):
     weigth_matrices.append(2*np.random.random((layer_lengths[i] + 1, layer_lengths[i+1])) - 1)
 
 # number of iterations
-num_iterations = 10000
+num_iterations = 100000
 # learning rate
 alpha = 0.1
 
-for i in range(num_iterations):
+for j in range(num_iterations):
     # output of each layer
     # outputs[0] - inputs with ones in first column (input for bias)
     # outputs[1] - output of first hidden layer, with ones in first column
@@ -92,16 +89,19 @@ for i in range(num_iterations):
     # Adjusting weights
     for i in range(0, num_layers-1):
         weigth_matrices[i] += -alpha * gradients[i]
+    
+    if j % 1000 == 0:
+        print("Error:", error_function(y, outputs[-1]))
 
 #%%
 result = []
-result.append(np.hstack((np.ones((X.shape[0], 1)), X)))
+result.append(np.hstack((np.ones((X_test.shape[0], 1)), X_test)))
 for i in range(0, num_layers-2):
-    result.append(np.hstack((np.ones((X.shape[0], 1)), activation(np.dot(result[i], weigth_matrices[i])))))
+    result.append(np.hstack((np.ones((X_test.shape[0], 1)), activation(np.dot(result[i], weigth_matrices[i])))))
 result.append(np.dot(result[-1], weigth_matrices[-1]))
 
 with np.printoptions(precision=3, suppress=True):
-    print("Expected output:\n", y)
-    print("Output After Training:\n", result[-1])
-    print("Error:\n", error_function(y, result[-1]))
+    print("Expected output:\n", y_test.T)
+    print("Output After Training:\n", result[-1].T)
+    print("Error:\n", error_function(y_test, result[-1]))
 # %%
