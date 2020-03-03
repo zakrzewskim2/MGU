ad04a8ce87ce21e3dacc39f73cb650ee4dedf4a0
Marcin Zakrzewski <zakrzewskim2@student.mini.pw.edu.pl>
2020-03-04 00:27:32 +0100

    softmax finally works
    Signed-off-by: Marcin Zakrzewski <zakrzewskim2@student.mini.pw.edu.pl>

--- NN.py
+++ NN.py
@@ -18,7 +18,7 @@ def softmax_derivative(x):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 def sigmoid_derivative(x):
-    return x * (1 - x) #sigmoid(x) * (1 - sigmoid(x))
+    return sigmoid(x) * (1 - sigmoid(x)) 
 def activation_function(x, type, derivative=False):
     if derivative:
         if type == ActivationFunction.SOFTMAX:
@@ -44,9 +44,9 @@ def error_function(y, y_exp, derivative = False):
         return (2 * (y - y_exp) / y_exp.shape[1])
     else:
         return ((y - y_exp)**2).mean()
-
+#%%
 def activation(x, derivative=False):
-    return activation_function(x,ActivationFunction.TANH, derivative)
+    return activation_function(x,ActivationFunction.SOFTMAX, derivative)
 
 def out_activation(x, derivative=False):
     return activation_function(x, ActivationFunction.LINEAR, derivative)
@@ -60,21 +60,13 @@ X_train = train.iloc[:,:-1]
 y_train = train["cls"]
 X_test = np.array(test.iloc[:,:-1])
 y_test = np.array([test["cls"]]).T
+
 # inputs - each row is an input to MLP
-X = np.array(X_train)#[0:4,:]
+s = 100
+X = np.array(X_train)[0:s,:]
 # outputs - each row is an expected output for the corresponding input
-y = np.array([y_train]).T#[0:4,:]
-# yy = np.zeros((100,2))
-# yy.T[0] = y.T[0]
-# yy.T[1] = y.T[0]
-# for i in range(100):
-#     if yy[i][0]==1:
-#         yy[i][0]=1
-#         yy[i][1]=0
-#     else:
-#         yy[i][0]=0
-#         yy[i][1]=1
-# y = yy
+y = np.array([y_train]).T[0:s,:]
+
 # N - number of input vectors
 N = X.shape[0]
 
@@ -91,10 +83,10 @@ np.random.seed(1)
 weight_matrices = []
 for i in range(0, num_layers-1):
     # Initial weights from [-1,1], with first row as bias
-    weight_matrices.append(2*np.random.random((layer_lengths[i] + 1, layer_lengths[i+1])) - 1)
+    weight_matrices.append(2*np.random.random((layer_lengths[i] + 1, layer_lengths[i+1]))-1)
 
 # number of iterations
-num_iterations = 10000
+num_iterations = 25000
 # learning rate
 alpha = 0.1
 
@@ -105,10 +97,14 @@ for j in range(num_iterations):
     # ...
     # outputs[-1] - output of last layer (without ones)
     outputs = []
+    not_activated_outputs = []
     outputs.append(np.hstack((np.ones((X.shape[0], 1)), X)))
+    not_activated_outputs.append(outputs[0])
     for i in range(0, num_layers-2):
         outputs.append(np.hstack((np.ones((X.shape[0], 1)), activation(np.dot(outputs[i], weight_matrices[i])))))
+        not_activated_outputs.append(np.hstack((np.ones((X.shape[0], 1)), np.dot(outputs[i], weight_matrices[i]))))
     outputs.append(np.dot(outputs[-1], weight_matrices[-1]))
+    not_activated_outputs.append(outputs[-1])
 
     # error term for each layer
     # errors[0] - error for last layer
@@ -116,27 +112,28 @@ for j in range(num_iterations):
     # ...
     # errors[-1] - error for first hidden layer
     errors = []
-    errors.append(out_activation(outputs[-1], True) * \
-        error_function(out_activation(outputs[-1]), out_activation(y), True))
+    err = (np.transpose(error_function(out_activation(outputs[-1]), y, True)[None,:,:],(0,2,1)) * \
+        np.transpose(out_activation(outputs[-1], True),(0,2,1))).sum(axis=1)
+    errors.append(err.T)
     for i in range(0, num_layers-2):
-        errors.append(activation(outputs[-i-2][:, 1:], True) * \
-            np.sum(np.dot(errors[i], weight_matrices[-i-1].T[:, 1:]), axis=0))
+        errors.append((np.dot(weight_matrices[-i-1][1:,:], errors[i].T)[None,:,:] * \
+            np.transpose(activation(not_activated_outputs[-i-2][:, 1:], True),(0,2,1))).sum(axis=1).T)
 
     # gradient for each matrix of weights
     # gradient[0] - gradient with respect to weights between input layer and first hidden layer
     # gradient[1] - gradient with respect to weights between first and second hidden layer
-    # ...
+    # ...   
     # gradient[-1] - gradient with respect to weights between last hidden layer and output layer
     gradients = []
     for i in range(0, num_layers-1):
-        gradients.append(np.sum(np.transpose(np.dot(outputs[i].T, errors[-i-1]),(1,0,2)), axis=0)/N)
+        gradients.append(np.dot(outputs[i].T, errors[-i-1])/N)
 
     # Adjusting weights
     for i in range(0, num_layers-1):
         weight_matrices[i] += -alpha * gradients[i]
-    
+
     if j % 1000 == 0:
-        print("Error:", error_function(y, outputs[-1]))
+        print("Error:", error_function(y, out_activation(outputs[-1])))
 
 #%%
 result = []
@@ -147,6 +144,6 @@ result.append(np.dot(result[-1], weight_matrices[-1]))
 
 with np.printoptions(precision=3, suppress=True):
     print("Expected output:\n", y_test.T)
-    print("Output After Training:\n", result[-1].T)
-    print("Error:\n", error_function(y_test, result[-1]))
+    print("Output After Training:\n", out_activation(result[-1]).T)
+    print("Error:\n", error_function(y_test, out_activation(result[-1])))
 # %%
