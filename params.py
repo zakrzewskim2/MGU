434bf53ba8971e470c51daed085fa755f2b3e0ba
Wawrzeńczyk Adam <adam.wawrzenczyk@gmail.com>
2020-03-06 14:16:24 +0100

    NN refactor

--- params.py
+++ params.py
@@ -7,14 +7,14 @@ class ProblemType(Enum):
     CLASSIFICATION = 1
     REGRESSION = 2
 
+class Config():
+    activation_function = activation_functions.softmax
+    error_function = error_functions.cross_entropy
 
-activation_function = activation_functions.softmax
-error_function = error_functions.cross_entropy
-
-hidden_layers = [5, 5, 3]
-bias = True
-batch_size = 0.3
-num_iterations = 100000
-eta = 0.1
-momentum = 0.1
-problem_type = ProblemType.CLASSIFICATION
+    hidden_layers = [5, 5, 3]
+    bias = True
+    batch_size = 0.3
+    num_iterations = 100000
+    eta = 0.1
+    moment = 0.1
+    problem_type = ProblemType.CLASSIFICATION
