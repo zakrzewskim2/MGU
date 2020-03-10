dd03d0527a430c791fdad7be172d84b9cb2adcfb
Adam Wawrzeńczyk <adam.wawrzenczyk@gmail.com>
2020-03-10 22:22:29 +0100

    Kaggle init

---
+++ digit_recognizer.py
@@ -0,0 +1,16 @@
+# %%
+import pandas as pd
+
+train = pd.read_csv("data/digits/train.csv.zip")
+test = pd.read_csv("data/digits/test.csv.zip")
+X_train, y_train = train.iloc[:, 1:], train.iloc[:, 0]
+X_test, y_test = test.iloc[:, 1:], test.iloc[:, 0]
+
+# %%
+from mlp.mlp_classifier import MLPClassifier
+
+clf = MLPClassifier()
+clf.fit(X_train, y_train).score(X_test, y_test)
+
+
+# %%
